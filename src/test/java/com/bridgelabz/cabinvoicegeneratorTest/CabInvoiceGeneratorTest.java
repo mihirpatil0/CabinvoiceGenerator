87e43988a83b689a94c4781899f32a909ed6d609
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-27 00:10:34 +0530

    [mihir] Added: calculating total fare for journey.

---
+++ src/test/java/com/bridgelabz/cabinvoicegeneratorTest/CabInvoiceGeneratorTest.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.cabinvoicegeneratorTest;
+
+import com.bridgelabz.cabinvoicegenerator.CabInvoiceGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/****************************************************
+ * @author mihir
+ *
+ * This is a test case class.
+ * This class holdes all the related test cases that,
+ * test all the methods od the project.
+ * **************************************************/
+
+public class CabInvoiceGeneratorTest
+{
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare()
+    {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 5.0;
+        double time = 2;
+        double totalfare =  cabInvoiceGenerator.getfare(distance,time);
+        Assertions.assertEquals(52, totalfare,0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_WhenLessThanMinimumFare_ShouldReturnTotalFare()
+    {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.1;
+        double time = 2;
+        double totalfare =  cabInvoiceGenerator.getfare(distance,time);
+        Assertions.assertEquals(5, totalfare,0.0);
+    }
+}
