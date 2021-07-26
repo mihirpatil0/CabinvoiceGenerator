c741764afe389f183f01f31544672f5808605bdb
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-27 00:45:02 +0530

    [mihir] Added: extended support for premium and normal ride.

--- src/test/java/com/bridgelabz/cabinvoicegeneratorTest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratorTest/CabInvoiceGeneratorTest.java
@@ -22,7 +22,7 @@ public class CabInvoiceGeneratorTest
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 5.0;
         double time = 2;
-        double totalfare =  cabInvoiceGenerator.getfare(distance,time);
+        double totalfare =  cabInvoiceGenerator.getfare(distance,time,"regular");
         Assertions.assertEquals(52, totalfare,0.0);
     }
 
@@ -32,7 +32,7 @@ public class CabInvoiceGeneratorTest
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 0.1;
         double time = 2;
-        double totalfare =  cabInvoiceGenerator.getfare(distance,time);
+        double totalfare =  cabInvoiceGenerator.getfare(distance,time,"regular");
         Assertions.assertEquals(5, totalfare,0.0);
     }
 
@@ -41,7 +41,7 @@ public class CabInvoiceGeneratorTest
     {
         CabInvoiceGenerator cabInvoiceGenerator =new CabInvoiceGenerator();
         Ride[] rides = { new Ride(5.0,2),new Ride(0.1,2)};
-        InvoiceSummary summary =  cabInvoiceGenerator.calculateTotalFare(1,rides);
+        InvoiceSummary summary =  cabInvoiceGenerator.calculateTotalFare(1,rides,"regular");
         InvoiceSummary  expectedSummary = new InvoiceSummary(1,2,57.0);
         Assertions.assertEquals(expectedSummary,summary);
     }
@@ -51,8 +51,28 @@ public class CabInvoiceGeneratorTest
     {
         CabInvoiceGenerator cabInvoiceGenerator =new CabInvoiceGenerator();
         Ride[] rides = { new Ride(5.0,2),new Ride(0.1,2)};
-        cabInvoiceGenerator.calculateTotalFare(1,rides);
+        cabInvoiceGenerator.calculateTotalFare(1,rides,"regular");
         Ride[] expectedRide = cabInvoiceGenerator.rideRepository.get(1);
         Assertions.assertEquals(expectedRide.toString(),rides.toString());
+    }
+
+    @Test
+    public void givenDistanceAndTime_WhenPremium_ShouldReturnTotalFare()
+    {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 2;
+        double time = 1;
+        double totalfare =  cabInvoiceGenerator.getfare(distance,time,"premium");
+        Assertions.assertEquals(32, totalfare,0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_WhenPremiumAndLessThanMinimum_ShouldReturnTotalFare()
+    {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 1;
+        double time = 1;
+        double totalfare =  cabInvoiceGenerator.getfare(distance,time,"premium");
+        Assertions.assertEquals(20, totalfare,0.0);
     }
 }
